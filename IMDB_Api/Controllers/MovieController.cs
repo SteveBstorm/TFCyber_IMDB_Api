5bcd02096688296b0db148200305f100d66f2377
stevebstorm <steve.lorent@gmail.Com>
2024-04-10 11:34:04 +0200

    Add swagger config

--- IMDB_Api/Controllers/MovieController.cs
+++ IMDB_Api/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using ASP_Demo_Archi_DAL.Repositories;
+﻿using ASP_Demo_Archi_DAL.Models;
+using ASP_Demo_Archi_DAL.Repositories;
 using ASP_Demo_Archi_DAL.Services;
 using IMDB_Api.Exemples;
 using IMDB_Api.Models;
@@ -20,25 +21,63 @@ namespace IMDB_Api.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
         public IActionResult GetAll()
         {
             //object truc = new { Nom = "steve", Age = 22, AgeMental = 12 };/
 
             return Ok(_movieRepo.GetAll()) ;
         }
-
+        /// <summary>
+        /// Faut pas respirer la compote, ça fait tousser
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Un objet de type Movie</returns>
+        /// <remarks>Cette méthode prend un Id de film en paramètre et 
+        /// retourne un Ok contenant l'objet</remarks>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
         public IActionResult GetById(int id)
         {
             return Ok(_movieRepo.GetById(id));
         }
 
+        
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(MovieCreateForm), StatusCodes.Status400BadRequest)]
+        
         public IActionResult Create(MovieCreateForm form)
         {
             if (!ModelState.IsValid) return BadRequest();
             _movieRepo.Create(form.ToDAL());
             return Ok();
         }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Delete(int id)
+        {
+            _movieRepo.Delete(id);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(MovieCreateForm), StatusCodes.Status400BadRequest)]
+
+        public IActionResult Update([FromBody]MovieCreateForm m, [FromRoute]int id)
+        {
+            if(!ModelState.IsValid) return BadRequest();
+            Movie movie = m.ToDAL();
+            movie.Id = id;
+            _movieRepo.Edit(movie);
+
+            return Ok();
+
+        }
+
     }
 }
--- IMDB_Api/Program.cs
+++ IMDB_Api/Program.cs
@@ -1,5 +1,7 @@
 using ASP_Demo_Archi_DAL.Repositories;
 using ASP_Demo_Archi_DAL.Services;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "IMDB Api",
+            Description = "Api fournissant des infos ciné",
+            Contact = new OpenApiContact
+            {
+                Name = "Steve Lorent",
+                Email = "steve.lorent@bstorm.be"
+            },
+            Version = "v1"
+        });
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+    });
 
 builder.Services.AddScoped<IMovieRepo, MovieService>();
 builder.Services.AddScoped<IPersonRepo, PersonService>();
