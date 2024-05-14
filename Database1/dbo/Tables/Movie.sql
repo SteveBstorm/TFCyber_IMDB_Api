a0957ba3bc8e3550de1665c43866e594f9d5c050
stevebstorm <steve.lorent@gmail.Com>
2024-05-14 15:50:02 +0200

    ajout projet db

---
+++ Database1/dbo/Tables/Movie.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Movie] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Title]        VARCHAR (100) NOT NULL,
+    [Description]  VARCHAR (500) NULL,
+    [RealisatorId] INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Movie_Real] FOREIGN KEY ([RealisatorId]) REFERENCES [dbo].[Person] ([Id])
+);
+
---
+++ Database1/dbo/Tables/Movie_Person.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Movie_Person] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [PersonId] INT           NOT NULL,
+    [MovieId]  INT           NOT NULL,
+    [Role]     VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Movie] FOREIGN KEY ([MovieId]) REFERENCES [dbo].[Movie] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([Id])
+);
+
